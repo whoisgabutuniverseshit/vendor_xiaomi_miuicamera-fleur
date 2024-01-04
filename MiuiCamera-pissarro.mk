b54f13613336df39890e1f22da33d37395ef9335
Alexander Winkowski <dereference23@outlook.com>
2024-01-04 17:50:37 +0000

    Allow MiuiCamera to read proximity and display type props
    Change-Id: Id646b72cdc04b12feacfaf90e08192a35f7a05e0

--- MiuiCamera-pissarro.mk
+++ MiuiCamera-pissarro.mk
@@ -10,6 +10,11 @@ $(call inherit-product, vendor/xiaomi/miuicamera/MiuiCamera.mk)
 PRODUCT_PACKAGES += \
     libcamera_algoup_jni.xiaomi
 
+# Properties
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.vendor.audio.us.proximity=true \
+    ro.vendor.display.type=oled
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     vendor/xiaomi/miuicamera-pissarro
